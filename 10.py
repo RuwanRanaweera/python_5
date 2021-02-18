4277c30e35323a7193c942b8ef25fdffa6276c36
ruwan <rwnranaweera@gmail.com>
2021-02-18 06:59:11 +0530

    no commit

---
+++ 10.py
@@ -0,0 +1,35 @@
+def callMe():
+    "print something"
+    print("Rane boy")
+    return
+
+callMe()
+
+
+def printS(str1):
+    '''dffdfsf
+ds
+dfdfs
+dfsf  '''
+    print(str1)
+    return
+
+printS("ndfsjcdl")
+printS("ndfsjcdl")
+printS("ndjcdl")
+printS("5555cdl")
+printS("ndds")
+
+def sum(a,b):
+    print(a+b)
+    return
+
+sum(5,4)
+sum(45.26,78.45)
+
+
+def sum(a,b):
+    print(a+b)
+    return
+
+sum(5,4)
---
+++ 11.py
@@ -0,0 +1,5 @@
+def printMe(str,str1):
+    print(str,str1)
+    return
+
+printMe(str="ruwan",str1="djcsckd")
